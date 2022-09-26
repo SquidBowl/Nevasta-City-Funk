382b193d5b2c133be83bed7b74a10e8ad58876a5
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-09-26 00:19:51 -0500

    Update evergreen.lua

--- NCF/FNF-PsychEngine-main/assets/preload/stages/evergreen.lua
+++ NCF/FNF-PsychEngine-main/assets/preload/stages/evergreen.lua
@@ -31,3 +31,4 @@ function onCreate()
     addLuaSprite('EGfronttree2', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end	
