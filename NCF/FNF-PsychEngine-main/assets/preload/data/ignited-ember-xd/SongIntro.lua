545ef21173b93ff798ba9d6d53bfa86b4d7d3e27
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-09-27 15:57:08 -0500

    Update SongIntro.lua

--- NCF/FNF-PsychEngine-main/assets/preload/data/ignited-ember-xd/SongIntro.lua
+++ NCF/FNF-PsychEngine-main/assets/preload/data/ignited-ember-xd/SongIntro.lua
@@ -16,7 +16,7 @@ function onCreate()
 	setTextSize('JukeBoxText', IntroTextSize)
 	addLuaText('JukeBoxText')
 	
-	makeLuaText('JukeBoxSubText', '  IE Encore', 300, -305-IntroTagWidth, 60)
+	makeLuaText('JukeBoxSubText', 'Ignited Ember XD', 300, -305-IntroTagWidth, 60)
 	setTextAlignment('JukeBoxSubText', 'left')
 	setObjectCamera('JukeBoxSubText', 'other')
 	setTextSize('JukeBoxSubText', IntroSubTextSize)
