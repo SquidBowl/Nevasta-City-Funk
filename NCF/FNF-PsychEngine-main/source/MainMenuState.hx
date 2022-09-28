ac7178fd96ddcd40d079588f205ab1acc23825a8
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-09-27 22:48:28 -0500

    Update MainMenuState.hx

--- NCF/FNF-PsychEngine-main/source/MainMenuState.hx
+++ NCF/FNF-PsychEngine-main/source/MainMenuState.hx
@@ -95,7 +95,7 @@ class MainMenuState extends MusicBeatState
 		// magenta.scrollFactor.set();
 
 		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
-		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('mm1'));
+		var bg:FlxSprite = new FlxSprite(80).loadGraphic(Paths.image('mm1'));
 		bg.scrollFactor.set(0, 0);
 		//bg.setGraphicSize(Std.int(bg.width * 1.175));
 		bg.updateHitbox();
