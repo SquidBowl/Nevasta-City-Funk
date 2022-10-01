650e5dd51704b8bb581cf760951da9d18f837ca5
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-09-30 21:07:06 -0500

    Update Side_HUD.lua

--- NCF/FNF-PsychEngine-main/example_mods/scripts/Side_HUD.lua
+++ NCF/FNF-PsychEngine-main/example_mods/scripts/Side_HUD.lua
@@ -50,21 +50,6 @@ function onCreate()
     setTextAlignment('miss', 'left')
 end
 
-
-function goodNoteHit(id, direction, noteType, isSustainNote)
-	-- Function called when you hit a note (after note hit calculations)
-	-- id: The note member id, you can get whatever variable you want from this note, example: "getPropertyFromGroup('notes', id, 'strumTime')"
-	-- noteData: 0 = Left, 1 = Down, 2 = Up, 3 = Right
-	-- noteType: The note type string/tag
-	-- isSustainNote: If it's a hold note, can be either true or false
-      if not isSustainNote then    
-         notehitlol = notehitlol + 1;
-         setTextString('tnh', 'Total Notes Hit: ' .. tostring(notehitlol))
-     end -- NOTE I DID NOT MAKE THIS FRANTASTIC24 MADE THIS!
-
-end
-
-
 function onUpdate(elapsed)
     notehitloltosting = tostring(notehitlol)
     setTextString('cm', 'Combos: ' .. getProperty('combo'))
