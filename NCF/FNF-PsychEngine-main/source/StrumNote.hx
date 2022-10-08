c4c583efb5c08b37a937df305f8ee8256732b360
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-10-08 03:24:53 -0500

    Update StrumNote.hx

--- NCF/FNF-PsychEngine-main/source/StrumNote.hx
+++ NCF/FNF-PsychEngine-main/source/StrumNote.hx
@@ -35,7 +35,6 @@ class StrumNote extends FlxSprite
 		super(x, y);
 
 		var skin:String = 'NOTE_assets';
-		var skin:String = 'NOTEXD_assets';
 		if(PlayState.SONG.arrowSkin != null && PlayState.SONG.arrowSkin.length > 1) skin = PlayState.SONG.arrowSkin;
 		texture = skin; //Load texture and anims
 
