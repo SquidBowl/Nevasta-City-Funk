c532068c7e1d1beca1fb3971adeb4a6585c47bc2
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-10-09 03:19:18 -0500

    Update Side_HUD.lua

--- NCF/FNF-PsychEngine-main/example_mods/scripts/Side_HUD.lua
+++ NCF/FNF-PsychEngine-main/example_mods/scripts/Side_HUD.lua
@@ -4,7 +4,7 @@ cmoffset = -4
 cmy = 20
 tnhx = -10
 function onCreate()
-    makeLuaText("tnh", '', 250, tnhx, 259);
+    makeLuaText("tnh", '', 250, tnhx, 459);
     setTextFont('tnh', font)
     makeLuaText("cm", 'Combo: 0', 200, -getProperty('tnh.x') + cmoffset, getProperty('tnh.y') + cmy);
     makeLuaText("sick", 'Fire!: 0', 200, getProperty('cm.x'), getProperty('cm.y') + 30);
