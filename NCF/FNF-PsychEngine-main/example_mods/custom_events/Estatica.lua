b820ecb2e2a73c5ef1433000de61d3f5af89ff18
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-10-17 02:24:31 -0500

    Create Estatica.lua

---
+++ NCF/FNF-PsychEngine-main/example_mods/custom_events/Estatica.lua
@@ -0,0 +1,25 @@
+function onCreate()
+    makeAnimatedLuaSprite('FlashingShitFatality','exestuff/Phase3Static', 0, 0)
+    addAnimationByPrefix('FlashingShitFatality','glitch','Phase3Static instance',24,true)
+    setProperty('FlashingShitFatality.alpha',0.9)
+    scaleObject('FlashingShitFatality',4,4)
+    setObjectCamera('FlashingShitFatality','hud')
+
+    debugPrint(getProperty('dad.x'))
+end
+
+function onEvent(name,value1)
+    if name == 'Trigger Static' then
+        addLuaSprite('FlashingShitFatality',true)
+        if value1 ~= '' and value1 ~= nil then
+            runTimer('destroyFlashShitfatality',value1/5)
+        else
+            runTimer('destroyFlashShitfatality',1.6)
+        end
+    end
+end
+function onTimerCompleted(tag)
+    if tag == 'destroyFlashShitfatality' then
+        removeLuaSprite('FlashingShitFatality',false)
+    end
+end
