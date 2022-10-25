cfc2953626d7910ad6cc175e5a3d8478e41635c9
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-10-24 20:00:58 -0500

    Update fantasma.lua

--- NCF/FNF-PsychEngine-main/assets/preload/stages/fantasma.lua
+++ NCF/FNF-PsychEngine-main/assets/preload/stages/fantasma.lua
@@ -2,7 +2,7 @@ function onCreate()
 	-- background shit
 	makeLuaSprite('normal', 'normal', -1050, -1000);
 	
-	makeLuaSprite('normal', 'normal', -550, -270);
+	makeLuaSprite('normal', 'normal', -400, -270);
 	scaleObject('normal', 1.7, 1.5);
 
 	addLuaSprite('normal', false);
