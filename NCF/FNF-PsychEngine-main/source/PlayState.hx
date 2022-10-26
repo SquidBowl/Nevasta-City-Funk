1de469d522a16f53256741fdf118445c3b19c82b
skypexvga13 <113746902+skypexvga13@users.noreply.github.com>
2022-10-26 03:04:22 -0500

    Update PlayState.hx

--- NCF/FNF-PsychEngine-main/source/PlayState.hx
+++ NCF/FNF-PsychEngine-main/source/PlayState.hx
@@ -713,7 +713,7 @@ class PlayState extends MusicBeatState
                 add(bg1floor);	
 
 			case 'bruh': //Forestal-Desire
-                var bg:BGSprite = new BGSprite('bruh', -1850, -480, 1.7, 1.5);
+                var bg:BGSprite = new BGSprite('bruh', -1550, -680, 1.7, 1.5);
                 add(bg);		
 		}
 
